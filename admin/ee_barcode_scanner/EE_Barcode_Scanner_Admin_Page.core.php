9c826c98462fe676ed703895287b92604799a667
Darren Ethier <darren@roughsmootheng.in>
2014-08-31 15:42:23 -0400

    add initial admin page setup [touch: 6568]

---
+++ admin/ee_barcode_scanner/EE_Barcode_Scanner_Admin_Page.core.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Contains Admin page class for barcode scanner admin page
+ *
+ * @since 1.0.0
+ * @package EE4 Barcode Scanner
+ * @subpackage admin
+ */
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) exit( 'No direct script access allowed' );
+
+/**
+ * Admin Page class for EE4 Barcode Scanner
+ *
+ * @since 1.0.0
+ * @see    EE_Admin_Page for all phpdocs related to core methods here that are not documented.
+ * @package EE4 Barcode Scanner
+ * @subpackage admin
+ * @author Darren Ethier
+ */
+class EE_Barcode_Scanner_Admin_Page extends EE_Admin_Page {
+
+	protected function _init_page_props() {
+		$this->page_slug = EE_BARCODE_SCANNER_PG_SLUG;
+		$this->_admin_base_url = EE_BARCODE_SCANNER_ADMIN_URL;
+		$this->_admin_base_path = EE_BARCODE_SCANNER_ADMIN;
+		$this->page_label = EE_BARCODE_SCANNER_PG_NAME;
+	}
+
+
+	protected function _define_page_props() {
+		$this->_admin_page_title = $this->page_label;
+		$this->_labels = array();
+	}
+
+
+	protected function _set_page_routes() {
+		$this->_page_routes = array(
+			'default' => array(
+				'func' => '_barcode_scanner',
+				'capability' => 'ee_edit_checkin'
+				)
+			);
+	}
+
+
+
+	protected function _set_page_config() {
+		$this->_page_config = array(
+			'default' => array(
+				'nav' => array(
+					'label' => __('Barcode Scanning', 'event_espresso' ),
+					'order' => 5
+					),
+				'help_tabs' => array(
+					'barcode_scanning_overview_help_tab' => array(
+						'title' => __('Overview', 'event_espresso'),
+						'filename' => 'barcode_scanner_overview'
+						)
+					)
+				)
+			);
+	}
+
+
+
+	//below in the next group are things that aren't used but have to be defined.
+	protected function _ajax_hooks() {}
+	protected function _add_screen_options() {}
+	protected function _add_feature_pointers() {}
+	public function admin_init() {}
+	public function admin_notices() {}
+	public function admin_footer_scripts() {}
+
+
+	public function load_scripts_styles() {
+		//@todo we'll load this from the EED_Barcode_Scanner module.
+	}
+
+
+
+	/**
+	 * Handles outputting the scanning form.
+	 * Note that this grabs the form and all related files etc from the EED_Barcode_Scanner module.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string html output.
+	 */
+	protected function _barcode_scanner() {
+		$this->_template_args['admin_page_content'] = 'Congratulations the Admin page is loaded.  Soon to be replaced with the actual scanner of course!';
+		$this->display_admin_page_with_no_sidebar();
+	}
+
+
+} //end EE_Barcode_Scanner_Admin_Page
---
+++ admin/ee_barcode_scanner/help_tabs/barcode_scanner_overview.help_tab.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Help Tab for barcode scanning default page.
+ */
+?>
+Congratulations, this file loaded correctly.  Obviously this is a todo, we need to document the scanner and how it works.  Will likely want to document the shortcode usage here as well I suppose.
