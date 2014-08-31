1f64461d2aa29cfd5eb69247b10a835152ea2343
Darren Ethier <darren@roughsmootheng.in>
2014-08-31 17:10:09 -0400

    template for scanner form [touch: 6568]

---
+++ admin/templates/scanner_detection_form.template.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This is the template for the scanner form.
+ *
+ * @since
+ *
+ * Template args available for this template are:
+ * @type string $_wpnonce  The created nonce for the scanning app.
+ * @type string $action_selector A selector for indicating the default actions when submitted.
+ */
+?>
+<!-- namespace with css -->
+<div class="eea-barcode-scanning-container">
+	<!-- barcode scanning form -->
+	<div class="eea-barcode-scanner-form-container">
+		<form name="eea-barcode-scan" action="" method="post">
+			<!-- hidden inputs -->
+			<input type="hidden" class="eea-barcode-scan-nonce" name="eea_barcode_scan_nonce" value="<?php echo $_wpnonce; ?>">
+
+			<!-- on with the form -->
+			<?php echo $action_selector; ?>
+			<input type="text" class="eea-barcode-scan-code" name="eea_barcode_scan_code" value="" placeholder="<?php _e('Scan or enter barcode here.', 'event_espresso' ); ?>">
+			<input type = "submit" value="<?php _e('Go', 'event_espresso'); ?>"
+		</form>
+	</div>
+</div>
+<div class="eea-barcode-scanning-results">
+</div>
