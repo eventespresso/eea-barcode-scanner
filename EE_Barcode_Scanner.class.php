cec345487d27a86fdf60e47e8f5bd9d1c246427f
Darren Ethier <darren@roughsmootheng.in>
2014-08-31 14:27:53 -0400

    modify EE_Barcode_Scanner class for proper setup [touch: 6568]

--- EE_Barcode_Scanner.class.php
+++ EE_Barcode_Scanner.class.php
@@ -1,13 +1,21 @@
-<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
+<?php
+/**
+ * File contains EE_Barcode_Scanner class (the starter for the engine).
+ *
+ * @since %VER%
+ * @package EE4 Barcode Scanner
+ * @subpackage scanning
+ */
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
 /**
  * ------------------------------------------------------------------------
  *
  * Class  EE_Barcode_Scanner
  *
- * @package			Event Espresso
- * @subpackage		espresso-ee-barcode-scanner
- * @author			    Brent Christensen
- * @ version		 	$VID:$
+ * @package			EE4 Barcode Scanner
+ * @subpackage		scanning
+ * @author			Darren Ethier
+ * @since %VER%
  *
  * ------------------------------------------------------------------------
  */
@@ -29,82 +37,31 @@ Class  EE_Barcode_Scanner extends EE_Addon {
 		EE_Register_Addon::register(
 			'EE_Barcode_Scanner',
 			array(
-				'version' 					=> EE_BARCODE_SCANNER_VERSION,
-				'min_core_version' => '4.3.0',
-				'main_file_path' 				=> EE_BARCODE_SCANNER_PLUGIN_FILE,
-				'admin_path' 			=> EE_BARCODE_SCANNER_ADMIN,
-				'admin_callback'		=> 'additional_admin_hooks',
-				'config_class' 			=> 'EE_Barcode_Scanner_Config',
-				'config_name' 		=> 'EE_Barcode_Scanner',
+				'version' => EE_BARCODE_SCANNER_VERSION,
+				'min_core_version' => '4.5.0',
+				'main_file_path' => EE_BARCODE_SCANNER_PLUGIN_FILE,
+				'admin_path' => EE_BARCODE_SCANNER_ADMIN,
 				'autoloader_paths' => array(
-					'EE_Barcode_Scanner' 						=> EE_BARCODE_SCANNER_PATH . 'EE_Barcode_Scanner.class.php',
-					'EE_Barcode_Scanner_Config' 			=> EE_BARCODE_SCANNER_PATH . 'EE_Barcode_Scanner_Config.php',
-					'EE_Barcode_Scanner_Admin_Page' 		=> EE_BARCODE_SCANNER_ADMIN . 'EE_Barcode_Scanner_Admin_Page.core.php',
+					'EE_Barcode_Scanner' => EE_BARCODE_SCANNER_PATH . 'EE_Barcode_Scanner.class.php',
+					'EE_Barcode_Scanner_Admin_Page' => EE_BARCODE_SCANNER_ADMIN . 'EE_Barcode_Scanner_Admin_Page.core.php',
 					'EE_Barcode_Scanner_Admin_Page_Init' => EE_BARCODE_SCANNER_ADMIN . 'EE_Barcode_Scanner_Admin_Page_Init.core.php',
 				),
-//				'dms_paths' 			=> array( EE_BARCODE_SCANNER_PATH . 'core' . DS . 'data_migration_scripts' . DS ),
-				'module_paths' 		=> array( EE_BARCODE_SCANNER_PATH . 'EED_EE_Barcode_Scanner.module.php' ),
-				'shortcode_paths' 	=> array( EE_BARCODE_SCANNER_PATH . 'EES_EE_Barcode_Scanner.shortcode.php' ),
-				'widget_paths' 		=> array( EE_BARCODE_SCANNER_PATH . 'EEW_EE_Barcode_Scanner.widget.php' ),
+				'module_paths' => array( EE_BARCODE_SCANNER_PATH . 'EED_EE_Barcode_Scanner.module.php' ),
+				'shortcode_paths' => array( EE_BARCODE_SCANNER_PATH . 'EES_EE_Barcode_Scanner.shortcode.php' ),
 				// if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
-				'pue_options'			=> array(
-					'pue_plugin_slug' => 'espresso_ee_barcode_scanner',
+				'pue_options' => array(
+					'pue_plugin_slug' => 'eea_barcode_scanner',
 					'checkPeriod' => '24',
 					'use_wp_update' => FALSE,
 					),
 				'capabilities' => array(
 					'administrator' => array(
-						'read_addon', 'edit_addon', 'edit_others_addon', 'edit_private_addon'
+						'ee_manage_scanner'
 						),
-					),
-				'capability_maps' => array(
-					new EE_Meta_Capability_Map_Edit( 'edit_addon', array( 'Event', '', 'edit_others_addon', 'edit_private_addon' ) )
-					),
-				'class_paths' => EE_BARCODE_SCANNER_PATH . 'core' . DS . 'db_classes',
-				'model_paths' => EE_BARCODE_SCANNER_PATH . 'core' . DS . 'db_models',
-				'class_extension_paths' => EE_BARCODE_SCANNER_PATH . 'core' . DS . 'db_class_extensions',
-				'model_extension_paths' => EE_BARCODE_SCANNER_PATH . 'core' . DS . 'db_model_extensions'
+					)
 			)
 		);
 	}
-
-
-
-	/**
-	 * 	additional_admin_hooks
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function additional_admin_hooks() {
-		// is admin and not in M-Mode ?
-		if ( is_admin() && ! EE_Maintenance_Mode::instance()->level() ) {
-			add_filter( 'plugin_action_links', array( $this, 'plugin_actions' ), 10, 2 );
-		}
-	}
-
-
-
-	/**
-	 * plugin_actions
-	 *
-	 * Add a settings link to the Plugins page, so people can go straight from the plugin page to the settings page.
-	 * @param $links
-	 * @param $file
-	 * @return array
-	 */
-	public function plugin_actions( $links, $file ) {
-		if ( $file == EE_BARCODE_SCANNER_BASENAME ) {
-			// before other links
-			array_unshift( $links, '<a href="admin.php?page=espresso_ee_barcode_scanner">' . __('Settings') . '</a>' );
-		}
-		return $links;
-	}
-
-
-
-
-
 
 }
 // End of file EE_Barcode_Scanner.class.php
