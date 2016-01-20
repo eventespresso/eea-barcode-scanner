2288ba3d6e2eaa713ffdbbe5750917452a76450c
Josh Feck <josh@nxnweb.com>
2016-01-20 15:28:04 -0500

    make strings i18n-ready

--- admin/barcode_scanner/help_tabs/barcode_scanner_overview.help_tab.php
+++ admin/barcode_scanner/help_tabs/barcode_scanner_overview.help_tab.php
@@ -3,9 +3,9 @@
  * Help Tab for barcode scanning default page.
  */
 ?>
-<p><strong>Scanning Barcodes</strong></p>
-<p><strong>Step 1:</strong> Click on the dropdown and locate your event and click on it.</p>
-<p><strong>Step 2:</strong> Decide if you want to continuously scan attendees/registrants or lookup an attendee and manually check them in.</p>
-<p><strong>Step 3:</strong> Once you’ve selected your event from the dropdown, use your mouse (or finger if using a mobile device) to select the field labeled “Scan or enter barcode here”, which listens for and reads the scanned barcodes.</p>
-<p><strong>Step 4:</strong> Scan a ticket! This is the fun part. Make sure your handheld scanner is plugged-in to your computer (or connected via bluetooth to your mobile device), then grab a ticket you’ve printed and scan it. If everything works correctly, the scanner should read the barcode and populate the text field with a number/text string that will automatically check-in or display an attendees information.</p>
-<p>For more details and examples, please <a href="https://eventespresso.com/wiki/barcode-scanner-add-on/">view the detailed documentation</a> for this feature</p>
+<p><strong><?php _e( 'Scanning Barcodes', 'event_espresso' ); ?></strong></p>
+<p><strong><?php _e( 'Step 1:', 'event_espresso' ); ?></strong> <?php _e( 'Click on the dropdown and locate your event and click on it.', 'event_espresso' ); ?></p>
+<p><strong><?php _e( 'Step 2:', 'event_espresso' ); ?></strong> <?php _e( 'Decide if you want to continuously scan attendees/registrants or lookup an attendee and manually check them in.', 'event_espresso' ); ?></p>
+<p><strong><?php _e( 'Step 3:', 'event_espresso' ); ?></strong> <?php _e( 'Once you’ve selected your event from the dropdown, use your mouse (or finger if using a mobile device) to select the field labeled “Scan or enter barcode here”, which listens for and reads the scanned barcodes.', 'event_espresso' ); ?></p>
+<p><strong><?php _e( 'Step 4:', 'event_espresso' ); ?></strong> <?php _e( 'Scan a ticket! This is the fun part. Make sure your handheld scanner is plugged-in to your computer (or connected via bluetooth to your mobile device), then grab a ticket you’ve printed and scan it. If everything works correctly, the scanner should read the barcode and populate the text field with a number/text string that will automatically check-in or display an attendees information.', 'event_espresso' ); ?></p>
+<p><?php printf( __( 'For more details and examples, please %sview the detailed documentation%s for this feature', 'event_espresso' ), '<a href="https://eventespresso.com/wiki/barcode-scanner-add-on/">', '</a>' ); ?></p>
--- admin/barcode_scanner/help_tabs/barcode_scanner_shortcode.help_tab.php
+++ admin/barcode_scanner/help_tabs/barcode_scanner_shortcode.help_tab.php
@@ -3,10 +3,23 @@
  * Help Tab for barcode scanning default page.
  */
 ?>
-<p><strong>Front-end Ticket Scanning Page</strong></p>
-<p>Tickets can be scanned from any page of your website by adding this shortcode to any WordPress page:<br>
+<p><strong><?php _e( 'Front-end Ticket Scanning Page', 'event_espresso' ); ?></strong></p>
+<p><?php _e( 'Tickets can be scanned from any page of your website by adding this shortcode to any WordPress page:', 'event_espresso' ); ?><br>
 <code>[ESPRESSO_BARCODE_SCANNER]</code>
 </p>
-<p>Please make sure the page is <strong>password protected</strong>, or set to private  to keep unwanted visitors from attempting to check themselves in to your events. See our note about permissions below.</p>
-<p><strong id="permissions">Note About Permissions:</strong> If the barcode scanner is loaded via the admin page, then we do a capability check to see if the user has the <code>ee_edit_checkin</code> capability before allowing any action to be done on the scanner (or to even show the scanner form). When this is used on a front facing page via a shortcode that capability check is completely removed. It can be filtered via the <code>EED_Barcode_Scanner__scanner_form__user_can_from_shortcode</code> filter to add a capability check for the front-end if desired, but it defaults to no capability check because that makes it easier for Event Admin’s to implement by just putting the barcode scanner on a password protected page.</p>
-<p>For more details and examples, please <a href="https://eventespresso.com/wiki/barcode-scanner-add-on/">view the detailed documentation</a> for this feature</p>
+<p><?php printf( 
+	__( 'Please make sure the page is %spassword protected%s, or set to private to keep unwanted visitors from attempting to check themselves in to your events. See our note about permissions below.', 'event_espresso' ), 
+	'<strong>', 
+	'</strong>' 
+); ?></p>
+<p><?php printf( 
+	__( '%1$sNote About Permissions:%2$s If the barcode scanner is loaded via the admin page, then we do a capability check to see if the user has the %3$s capability before allowing any action to be done on the scanner (or to even show the scanner form). When this is used on a front facing page via a shortcode that capability check is completely removed. It can be filtered via the %4$s filter to add a capability check for the front-end if desired, but it defaults to no capability check because that makes it easier for Event Admins to implement by just putting the barcode scanner on a password protected page.', 'event_espresso' ), 
+	'<strong id="permissions">',
+	'</strong>',
+	'<code>ee_edit_checkin</code>',
+	'<code>EED_Barcode_Scanner__scanner_form__user_can_from_shortcode</code>'
+); ?></p>
+<p><?php printf( __( 'For more details and examples, please %sview the detailed documentation%s for this feature.', 'event_espresso' ),
+	'<a href="https://eventespresso.com/wiki/barcode-scanner-add-on/">',
+	'</a>'
+); ?></p>
