2c3ce50c9543711d2df315941e334430a8eaad85
Darren Ethier <darren@roughsmootheng.in>
2018-05-06 18:30:10 -0400

    fix all phpcs violations (#4)

--- admin/barcode_scanner/Barcode_Scanner_Admin_Page.core.php
+++ admin/barcode_scanner/Barcode_Scanner_Admin_Page.core.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Contains Admin page class for barcode scanner admin page
- *
- * @since 1.0.0
- * @package EE4 Barcode Scanner
- * @subpackage admin
- */
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) exit( 'No direct script access allowed' );
 
 /**
  * Admin Page class for EE4 Barcode Scanner
@@ -17,85 +9,104 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) exit( 'No direct script access allo
  * @subpackage admin
  * @author Darren Ethier
  */
-class Barcode_Scanner_Admin_Page extends EE_Admin_Page {
+class Barcode_Scanner_Admin_Page extends EE_Admin_Page
+{
 
-	protected function _init_page_props() {
-		$this->page_slug = EE_BARCODE_SCANNER_PG_SLUG;
-		$this->_admin_base_url = EE_BARCODE_SCANNER_ADMIN_URL;
-		$this->_admin_base_path = EE_BARCODE_SCANNER_ADMIN;
-		$this->page_label = EE_BARCODE_SCANNER_PG_NAME;
-	}
-
-
-	protected function _define_page_props() {
-		$this->_admin_page_title = $this->page_label;
-		$this->_labels = array();
-	}
+    protected function _init_page_props()
+    {
+        $this->page_slug = EE_BARCODE_SCANNER_PG_SLUG;
+        $this->_admin_base_url = EE_BARCODE_SCANNER_ADMIN_URL;
+        $this->_admin_base_path = EE_BARCODE_SCANNER_ADMIN;
+        $this->page_label = EE_BARCODE_SCANNER_PG_NAME;
+    }
 
 
-	protected function _set_page_routes() {
-		$this->_page_routes = array(
-			'default' => array(
-				'func' => '_barcode_scanner',
-				'capability' => 'ee_read_checkins'
-				)
-			);
-	}
+    protected function _define_page_props()
+    {
+        $this->_admin_page_title = $this->page_label;
+        $this->_labels = array();
+    }
 
 
-
-	protected function _set_page_config() {
-		$this->_page_config = array(
-			'default' => array(
-				'nav' => array(
-					'label' => __('Barcode Scanning', 'event_espresso' ),
-					'order' => 5
-					),
-				'help_tabs' => array(
-					'barcode_scanning_overview_help_tab' => array(
-						'title' => __('Overview', 'event_espresso'),
-						'filename' => 'barcode_scanner_overview'
-						),
-					'barcode_scanning_shortcode_help_tab' => array(
-						'title' => __('Front-end Ticket Scanning', 'event_espresso'),
-						'filename' => 'barcode_scanner_shortcode'
-						)
-					)
-				)
-			);
-	}
+    protected function _set_page_routes()
+    {
+        $this->_page_routes = array(
+            'default' => array(
+                'func' => '_barcode_scanner',
+                'capability' => 'ee_read_checkins'
+                )
+            );
+    }
 
 
 
-	//below in the next group are things that aren't used but have to be defined.
-	protected function _ajax_hooks() {}
-	protected function _add_screen_options() {}
-	protected function _add_feature_pointers() {}
-	public function admin_init() {}
-	public function admin_notices() {}
-	public function admin_footer_scripts() {}
-	public function load_scripts_styles() {}
+    protected function _set_page_config()
+    {
+        $this->_page_config = array(
+            'default' => array(
+                'nav' => array(
+                    'label' => __('Barcode Scanning', 'event_espresso'),
+                    'order' => 5
+                    ),
+                'help_tabs' => array(
+                    'barcode_scanning_overview_help_tab' => array(
+                        'title' => __('Overview', 'event_espresso'),
+                        'filename' => 'barcode_scanner_overview'
+                        ),
+                    'barcode_scanning_shortcode_help_tab' => array(
+                        'title' => __('Front-end Ticket Scanning', 'event_espresso'),
+                        'filename' => 'barcode_scanner_shortcode'
+                        )
+                    )
+                )
+            );
+    }
 
 
 
-	public function load_scripts_styles_default() {
-		EED_Barcode_Scanner::instance()->enqueue_scripts();
-	}
+    // below in the next group are things that aren't used but have to be defined.
+    protected function _ajax_hooks()
+    {
+    }
+    protected function _add_screen_options()
+    {
+    }
+    protected function _add_feature_pointers()
+    {
+    }
+    public function admin_init()
+    {
+    }
+    public function admin_notices()
+    {
+    }
+    public function admin_footer_scripts()
+    {
+    }
+    public function load_scripts_styles()
+    {
+    }
 
 
 
-	/**
-	 * Handles outputting the scanning form.
-	 * Note that this grabs the form and all related files etc from the EED_Barcode_Scanner module.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return string html output.
-	 */
-	protected function _barcode_scanner() {
-		$this->_template_args['admin_page_content'] = EED_Barcode_Scanner::instance()->scanner_form( false );
-		$this->display_admin_page_with_no_sidebar();
-	}
+    public function load_scripts_styles_default()
+    {
+        EED_Barcode_Scanner::instance()->enqueue_scripts();
+    }
 
 
-} //end EE_Barcode_Scanner_Admin_Page
+
+    /**
+     * Handles outputting the scanning form.
+     * Note that this grabs the form and all related files etc from the EED_Barcode_Scanner module.
+     *
+     * @since 1.0.0
+     *
+     * @return string html output.
+     */
+    protected function _barcode_scanner()
+    {
+        $this->_template_args['admin_page_content'] = EED_Barcode_Scanner::instance()->scanner_form(false);
+        $this->display_admin_page_with_no_sidebar();
+    }
+}
--- admin/barcode_scanner/templates/scanner_detection_form.template.php
+++ admin/barcode_scanner/templates/scanner_detection_form.template.php
@@ -12,7 +12,7 @@
  * @type string $event_selector  If present then the event selector is shown.
  * @type string $dtt_selector   If there is only one event, then this will contain the datetimes on that event (if multiples datetimes.)
  * @type string $dtt_name       If there is only one datetime then this will have the name of that datetime.
- * @type int     $dtt_id 		If there is only one datetime then this will have the id of the dtt.
+ * @type int     $dtt_id        If there is only one datetime then this will have the id of the dtt.
  * @type string $action_selector A selector for indicating the default actions when submitted.
  * @type string $button_class The class for the submit button.
  * @type string $checkin_link The link to the check in list table for this event and datetime.
@@ -20,76 +20,77 @@
 $eeactivestep1 = $step === 1 ? ' eea-bs-step-active' : '';
 $eeactivestep2 = $step === 2 ? ' eea-bs-step-active' : '';
 $eeactivestep3 = $step === 3 ? ' eea-bs-step-active' : '';
-$eventdisplay = empty( $event_name ) ? ' style="display:none;"' : '';
-$dtt_display = empty( $dtt_name ) ? ' style="display:none;"' : '';
-$checkin_link_display = empty( $checkin_link ) ? ' style="display:none;"' : '';
-$divider_display = empty( $dtt_name ) ? ' style="display:none;"' : '';
-$scanner_display = !empty( $dtt_name ) && ! empty( $event_name ) ? '' : ' style="display:none;"';
-$referrer = site_url( esc_attr( wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
+$eventdisplay = empty($event_name) ? ' style="display:none;"' : '';
+$dtt_display = empty($dtt_name) ? ' style="display:none;"' : '';
+$checkin_link_display = empty($checkin_link) ? ' style="display:none;"' : '';
+$divider_display = empty($dtt_name) ? ' style="display:none;"' : '';
+$scanner_display = !empty($dtt_name) && ! empty($event_name) ? '' : ' style="display:none;"';
+$referrer = site_url(esc_attr(wp_unslash($_SERVER['REQUEST_URI'])));
 $frontend_class = is_admin() ? '' : ' eea-barcode-scanner-frontend';
 ?>
 <!-- namespace with css -->
 <div class="eea-barcode-scanning-container<?php echo $frontend_class; ?>">
-	<!-- notices -->
-	<div class="eea-barcode-notices"><!--used by js --></div>
-	<span class="js-http-referrer" style="display:none"><?php echo $referrer; ?></span>
-	<!-- step display -->
-	<div class="eea-bs-main-step-container">
-		<div data-step-num="1" class="eea-bs-step-number eea-bs-step-1<?php echo $eeactivestep1; ?>"><div class="eea-step-bubble"><p>1</p></div><span class="eea-bs-step-text"><?php _e('Choose Event', 'event_espresso'); ?></span></div>
-		<div data-step-num="2" class="eea-bs-step-number eea-bs-step-2<?php echo $eeactivestep2; ?>"><div class="eea-step-bubble"><p>2</p></div><span class="eea-bs-step-text"><?php _e('Choose Date-time', 'event_espresso'); ?></span></div>
-		<div data-step-num="3" class="eea-bs-step-number eea-bs-step-3<?php echo $eeactivestep3; ?>"><div class="eea-step-bubble"><p>3</p></div><span class="eea-bs-step-text"><?php _e('Scan', 'event_espresso'); ?></span></div>
-		<span class="js-current-step-on-init" style="display:none;"><?php echo $step; ?></span>
-	</div>
-	<div class="eea-bs-ed-selection-container">
-		<div class="eea-bs-ed-selector eea-bs-event-selection">
-			<?php echo $event_selector; ?>
-			<div class="eea-bs-ed-selector-selected-text">
-				<h3 class="eea-bs-ed-selected-event-text"<?php echo $eventdisplay; ?>><?php echo $event_name; ?></h3>
-			</div>
-			<span class="spinner"></span>
-		</div>
-		<!--<div class="eea-bs-ed-selector">
-			<span class="eea-bs-ed-selector-divider"<?php echo $divider_display; ?>></span>
-		</div> -->
-		<div class="eea-bs-ed-selector eea-bs-dtt-selection">
-			<div class="eea-bs-dtt-selection-container">
-				<?php echo $dtt_selector; ?>
-			</div>
-			<?php if ( ! empty( $dtt_id ) ) : ?>
-				<input type="hidden" id="eea_bs_dtt_selector_hidden" name="eea_bs_dtt_selector_hidden" value="<?php echo $dtt_id; ?>">
-			<?php endif; ?>
-			<div class="eea-bs-ed-selector-selected-text">
-				<h3 class="eea-bs-ed-selected-dtt-text"<?php echo $dtt_display; ?>><?php echo $dtt_name; ?></h3>
-			</div>
-			<span class="spinner"></span>
-		</div>
-		<?php if ( is_admin() ) : ?>
-			<div class="eea-bs-ed-checkin-link-container">
-				<a target="_blank" href="<?php echo $checkin_link; ?>" class="eea-bs-ed-checkin-link"<?php echo $checkin_link_display; ?>><?php _e('View All Registrations', 'event_espresso'); ?></a>
-			</div>
-		<?php endif; ?>
-	</div>
-	<!-- barcode scanning form -->
-	<div class="eea-barcode-scanner-form-container"<?php echo $scanner_display; ?>>
-		<form id="eea-barcode-scanner-form" name="eea-barcode-scan" action="" method="post">
-			<!-- hidden inputs -->
-			<input type="hidden" id="eea-barcode-scan-nonce" name="eea_barcode_scan_nonce" value="<?php echo $_wpnonce; ?>">
-			<input type="hidden" id="eea-barcode-scan-context" name="eea_barcode_scan_context" value="<?php echo $context; ?>">
-			<input type="hidden" id="eea-barcode-scan-base-url" name="eea-barcode_scan_base_url" value="<?php echo admin_url(); ?>">
+    <!-- notices -->
+    <div class="eea-barcode-notices"><!--used by js --></div>
+    <span class="js-http-referrer" style="display:none"><?php echo $referrer; ?></span>
+    <!-- step display -->
+    <div class="eea-bs-main-step-container">
+        <div data-step-num="1" class="eea-bs-step-number eea-bs-step-1<?php echo $eeactivestep1; ?>"><div class="eea-step-bubble"><p>1</p></div><span class="eea-bs-step-text"><?php _e('Choose Event', 'event_espresso'); ?></span></div>
+        <div data-step-num="2" class="eea-bs-step-number eea-bs-step-2<?php echo $eeactivestep2; ?>"><div class="eea-step-bubble"><p>2</p></div><span class="eea-bs-step-text"><?php _e('Choose Date-time', 'event_espresso'); ?></span></div>
+        <div data-step-num="3" class="eea-bs-step-number eea-bs-step-3<?php echo $eeactivestep3; ?>"><div class="eea-step-bubble"><p>3</p></div><span class="eea-bs-step-text"><?php _e('Scan', 'event_espresso'); ?></span></div>
+        <span class="js-current-step-on-init" style="display:none;"><?php echo $step; ?></span>
+    </div>
+    <div class="eea-bs-ed-selection-container">
+        <div class="eea-bs-ed-selector eea-bs-event-selection">
+            <?php echo $event_selector; ?>
+            <div class="eea-bs-ed-selector-selected-text">
+                <h3 class="eea-bs-ed-selected-event-text"<?php echo $eventdisplay; ?>><?php echo $event_name; ?></h3>
+            </div>
+            <span class="spinner"></span>
+        </div>
+        <!--<div class="eea-bs-ed-selector">
+            <span class="eea-bs-ed-selector-divider"<?php echo $divider_display; ?>></span>
+        </div> -->
+        <div class="eea-bs-ed-selector eea-bs-dtt-selection">
+            <div class="eea-bs-dtt-selection-container">
+                <?php echo $dtt_selector; ?>
+            </div>
+            <?php if (! empty($dtt_id)) : ?>
+                <input type="hidden" id="eea_bs_dtt_selector_hidden" name="eea_bs_dtt_selector_hidden" value="<?php echo $dtt_id; ?>">
+            <?php endif; ?>
+            <div class="eea-bs-ed-selector-selected-text">
+                <h3 class="eea-bs-ed-selected-dtt-text"<?php echo $dtt_display; ?>><?php echo $dtt_name; ?></h3>
+            </div>
+            <span class="spinner"></span>
+        </div>
+        <?php if (is_admin()) : ?>
+            <div class="eea-bs-ed-checkin-link-container">
+                <a target="_blank" href="<?php echo $checkin_link; ?>" class="eea-bs-ed-checkin-link"<?php echo $checkin_link_display; ?>><?php _e('View All Registrations', 'event_espresso'); ?></a>
+            </div>
+        <?php endif; ?>
+    </div>
+    <!-- barcode scanning form -->
+    <div class="eea-barcode-scanner-form-container"<?php echo $scanner_display; ?>>
+        <form id="eea-barcode-scanner-form" name="eea-barcode-scan" action="" method="post">
+            <!-- hidden inputs -->
+            <input type="hidden" id="eea-barcode-scan-nonce" name="eea_barcode_scan_nonce" value="<?php echo $_wpnonce; ?>">
+            <input type="hidden" id="eea-barcode-scan-context" name="eea_barcode_scan_context" value="<?php echo $context; ?>">
+            <input type="hidden" id="eea-barcode-scan-base-url" name="eea-barcode_scan_base_url" value="<?php echo admin_url(); ?>">
 
-			<!-- on with the form -->
-			<?php echo $action_selector; ?>
-			<input type="text" class="eea-barcode-scan-code" name="eea_barcode_scan_code" value="" placeholder="<?php _e('Scan or enter barcode here.', 'event_espresso' ); ?>">
-			<input class="<?php echo $button_class; ?>" type="submit" value="<?php _e('Go', 'event_espresso'); ?>">
-		</form>
-		<span class="spinner"></span>
-		<div style="clear:both"></div>
-	</div>
-	<div class="eea-barcode-scanning-results">
-		<!-- used by js -->
-		<?php if ( !empty( $reg_content ) ) {
-			echo $reg_content;
-		}
-		?>
-	</div>
+            <!-- on with the form -->
+            <?php echo $action_selector; ?>
+            <input type="text" class="eea-barcode-scan-code" name="eea_barcode_scan_code" value="" placeholder="<?php _e('Scan or enter barcode here.', 'event_espresso'); ?>">
+            <input class="<?php echo $button_class; ?>" type="submit" value="<?php _e('Go', 'event_espresso'); ?>">
+        </form>
+        <span class="spinner"></span>
+        <div style="clear:both"></div>
+    </div>
+    <div class="eea-barcode-scanning-results">
+        <!-- used by js -->
+        <?php
+        if (!empty($reg_content)) {
+            echo $reg_content;
+        }
+        ?>
+    </div>
 </div>
