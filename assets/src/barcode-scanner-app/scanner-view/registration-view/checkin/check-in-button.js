93f6568e302bef94c6e4a497dcaf954c5c447d50
Darren Ethier <darren@roughsmootheng.in>
2019-05-03 19:40:10 -0400

    update checkin button

--- assets/src/barcode-scanner-app/scanner-view/registration-view/checkin/check-in-button.js
+++ assets/src/barcode-scanner-app/scanner-view/registration-view/checkin/check-in-button.js
@@ -4,20 +4,40 @@
 import { Button } from '@wordpress/components';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import { useCallback } from '@wordpress/element';
 
 import {
 	getCheckInActionText,
 	getCheckInActionClassName,
 } from './get-check-in-status-configuration';
 
-const CheckInButton = ( { checkinEntity, onClick } ) => {
+/**
+ * A component outputting a button for the given checkIn entity.
+ *
+ * The button will have its coloring and action text presented from the context
+ * of toggling the state of the given checkIn Entity.
+ *
+ * @param {BaseEntity|null} checkinEntity  A checkin entity or null (which
+ * means not checked in yet.
+ * @param {function} onClick  The callback to fire when the button is clicked.
+ * @param {boolean}  force	  If true, then when the checkin entity is null or
+ * has a status of checked out, this allows for an action that ignores checkin
+ * restrictions.
+ * @return {*}  The component rendered.
+ * @constructor
+ */
+const CheckInButton = ( { checkinEntity, onClick, force } ) => {
+	const clickHandler = useCallback(
+		() => onClick( force ),
+		[ checkinEntity, force ]
+	);
 	const buttonText = getCheckInActionText( checkinEntity );
 	const cssClass = classnames(
-		getCheckInActionClassName( checkinEntity ),
+		getCheckInActionClassName( checkinEntity, force ),
 		'ee-button',
 		'ee-roundish'
 	);
-	return <Button onClick={ onClick } className={ cssClass }>
+	return <Button onClick={ clickHandler } className={ cssClass }>
 		{ buttonText }
 	</Button>;
 };
@@ -25,11 +45,13 @@ const CheckInButton = ( { checkinEntity, onClick } ) => {
 CheckInButton.propTypes = {
 	checkinEntity: PropTypes.object,
 	onClick: PropTypes.func,
+	force: PropTypes.bool
 };
 
 CheckInButton.defaultProps = {
 	checkinEntity: null,
 	onClick: () => null,
+	force: false,
 };
 
 export default CheckInButton;
